085d0efc9280bea937bc798392ff43fdd30fe587
jordi <jordi.lanau@yahoo.com>
2021-11-21 18:10:40 +0000

    Display current weather conditions

---
+++ js/utils/urlData.js
@@ -0,0 +1,13 @@
+const key = '69c29afa16095d75381b27ad1495eb93';
+
+export function getWeatherURL(city) {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
+}
+
+export function getUVIURL(lon, lat) {
+  return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${key}`;
+}
+
+export function getForecastURL(city) {
+  return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${key}`;
+}
